727758d815297fc96a9089460a08410fe9b8e0d9
priyankraychura <priyankraychura@gmail.com>
2024-06-19 15:56:43 +0530

    New method used to char.c

--- DAY-14/char.c
+++ DAY-14/char.c
@@ -3,15 +3,23 @@
 int main(){
     int count = 0;
 
-    for (char i = 'A'; i <= 'Z'; i++)
+    // for (char i = 'A'; i <= 'Z'; i++)
+    // {
+    //     if (count < 3){
+    //         count++;
+    //         continue;
+    //     }
+    //     printf("%c ", i);
+    //     count = 0;
+    // }  
+
+    char num = 'A';
+
+    while (num <= 'Z')
     {
-        if (count < 3){
-            count++;
-            continue;
-        }
-        printf("%c ", i);
-        count = 0;
-    }  
+        printf("%c ", num);
+        num = num + 4;
+    }
 
     return 0;
 }
